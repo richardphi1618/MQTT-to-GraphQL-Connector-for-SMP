144ea0bc2dd9e1657d1b5284976be695060f775c
richard <richardphi1618@gmail.com>
2021-07-16 10:09:04 -0400

    refactoring for readability and looking into scaling issues

--- SMplatform.py
+++ SMplatform.py
@@ -1,16 +1,25 @@
-import requests
-import json, csv, os, dotenv
+import csv
+import os
 
-def request(content, url, headers=None, verbose = False):
-    if(verbose == True): print(content)
+import dotenv
+import requests
+
+
+def request(content: str, url: str, headers: str = None, verbose: bool = False) -> dict:
+    """Execute request with verbose wrapper"""
+    if verbose:
+        print(content)
     r = requests.post(url, headers=headers, data={"query": content})
     r.raise_for_status()
-    if(verbose):print(r.json())
+    if verbose:
+        print(r.json())
     return r.json()
 
-def get_token (auth, password, name, url, role):
-    
-    response = request(f'''
+
+def get_token(auth: str, password: str, name: str, url: str, role: str) -> str:
+    """Get JWT"""
+    response = request(
+        f"""
     mutation authRequest {{
         authenticationRequest(input: {{authenticator: "{auth}", role: "{role}", userName: "{name}"}}) {{
             jwtRequest {{
@@ -18,27 +27,36 @@ def get_token (auth, password, name, url, role):
             }}
         }}
     }}
-    ''', url) 
+    """,
+        url,
+    )
 
-    jwt_request = response['data']['authenticationRequest']['jwtRequest']
+    jwt_request = response["data"]["authenticationRequest"]["jwtRequest"]
 
-    if jwt_request['challenge'] is None:
-        raise Exception(jwt_request['message'])
+    if jwt_request["challenge"] is None:
+        raise Exception(jwt_request["message"])
     else:
-        response=request(f'''
+        response = request(
+            f"""
         mutation authValidation {{
-            authenticationValidation( input: {{authenticator: "{auth}", signedChallenge: "{jwt_request["challenge"]}|{password}"}} ){{
-                jwtClaim
-            }}
+            authenticationValidation( input: {{
+                authenticator: "{auth}", signedChallenge: "{jwt_request["challenge"]}|{password}"
+                }} )
+                {{jwtClaim}}
         }}
-        ''', url)
+        """,
+            url,
+        )
 
-    jwt_claim = response['data']['authenticationValidation']['jwtClaim']
+    jwt_claim = response["data"]["authenticationValidation"]["jwtClaim"]
     return f"Bearer {jwt_claim}"
 
-def build_tsData_Query(tagID, start = "2020-12-1T00:00:00", end = "now", maxSamples=0):
 
-    query_string = f'''
+def build_tsData_Query(
+    tagID: str, start: str = "2020-12-1T00:00:00", end: str = "now", maxSamples: int = 0
+) -> str:
+    """Build query for time series data"""
+    query_string = f"""
     query tsData_Query {{
         getRawHistoryDataWithSampling(
             startTime: "{start}"
@@ -55,11 +73,15 @@ def build_tsData_Query(tagID, start = "2020-12-1T00:00:00", end = "now", maxSamp
             dataType
         }}
     }}
-    '''
+    """
     return query_string
 
-def build_RunIDts_Query(runID , startTime = "2020-12-1T00:00:00", endTime = "now"):
-    query_string = f'''
+
+def build_RunIDts_Query(
+    runID: str, startTime: str = "2020-12-1T00:00:00", endTime: str = "now"
+) -> str:
+    """Build query for Run ID data"""
+    query_string = f"""
     query RunIDts_Query {{
         getRawHistoryDataWithSampling(
             ids: "6530"
@@ -72,28 +94,45 @@ def build_RunIDts_Query(runID , startTime = "2020-12-1T00:00:00", endTime = "now
         stringvalue
         }}
     }}
-    '''
+    """
     return query_string
 
-def getStartandEndTime(runID, endpoint_url, header, verbose=False):
-    StartandEnd=['','']
 
-    my_query = build_RunIDts_Query(f"equalTo: \"{runID}\" ")
-    if(verbose):print(my_query)
-    result = request(my_query, endpoint_url, header) # Execute the query
-    StartandEnd[0]= result['data']['getRawHistoryDataWithSampling'][0]['ts'] #save the start time
-    if(verbose):print(result)
+def getStartandEndTime(
+    runID: str, endpoint_url: str, header: dict, verbose: bool = False
+) -> list[str]:
+    """Get start and end times for specific runID"""
+    StartandEnd = ["", ""]
 
-    my_query = build_RunIDts_Query("equalTo: \"0\"",StartandEnd[0]) #search for the finish
-    if(verbose):print(my_query)
-    result = request(my_query, endpoint_url, header) # Execute the query
-    StartandEnd[1]= result['data']['getRawHistoryDataWithSampling'][0]['ts']
-    if(verbose):print(result)
+    my_query = build_RunIDts_Query(f'equalTo: "{runID}" ')
+    if verbose:
+        print(my_query)
+
+    # Execute the query
+    result = request(my_query, endpoint_url, header)
+
+    # save the start time
+    StartandEnd[0] = result["data"]["getRawHistoryDataWithSampling"][0]["ts"]
+    if verbose:
+        print(result)
+
+    # search for the finish
+    my_query = build_RunIDts_Query('equalTo: "0"', StartandEnd[0])
+    if verbose:
+        print(my_query)
+
+    # Execute the query
+    result = request(my_query, endpoint_url, header)
+    StartandEnd[1] = result["data"]["getRawHistoryDataWithSampling"][0]["ts"]
+    if verbose:
+        print(result)
 
     return StartandEnd
 
-def build_TagList_Query(name):
-    query_string = f'''
+
+def build_TagList_Query(name: str) -> str:
+    """Build query for returning tag list"""
+    query_string = f"""
     query TagList_Query {{
         tags(filter: {{ displayName: {{ startsWith: "{name}" }} }}) {{
             id
@@ -102,11 +141,15 @@ def build_TagList_Query(name):
             dataType
         }}
     }}
-    '''
+    """
     return query_string
 
-def build_CreateTag_Mutation(name, dataType="STRING", desc = "", partOfId = "214"):
-    query_string = f'''
+
+def build_CreateTag_Mutation(
+    name: str, dataType: str = "STRING", desc: str = "", partOfId: str = "214"
+) -> str:
+    """Build mutation for creating a new tag"""
+    query_string = f"""
     mutation CreateTag_Mutation {{
         createTag(
             input: {{
@@ -122,11 +165,15 @@ def build_CreateTag_Mutation(name, dataType="STRING", desc = "", partOfId = "214
         clientMutationId
         }}
     }}
-    '''
+    """
     return query_string
 
-def build_UpdateTagTS_Mutation(tagID, value, time="now", status="0"):
-    query_string = f'''
+
+def build_UpdateTagTS_Mutation(
+    tagID: str, value: str, time: str = "now", status: str = "0"
+) -> str:
+    """Build mutation for updating time series for a specific tag ID"""
+    query_string = f"""
     mutation UpdateTagTS_Mutation {{
         replaceTimeSeriesRange(
             input: {{
@@ -138,11 +185,14 @@ def build_UpdateTagTS_Mutation(tagID, value, time="now", status="0"):
           json
         }}
     }}
-    '''
+    """
     return query_string
 
-def build_UpdateMultipleTagTS_Mutation(tagID, entries):
-    query_string = f'''
+
+def build_UpdateMultipleTagTS_Mutation(tagID: str, entries: str) -> str:
+    """Build mutation for updating multiple tags with multiple timeseries data"""
+    # this came about through experimentation. Instead of deleting build_UpdateTagTS_Mutation I kept it
+    query_string = f"""
     mutation UpdateTagTS_Mutation {{
         replaceTimeSeriesRange(
             input: {{
@@ -154,116 +204,142 @@ def build_UpdateMultipleTagTS_Mutation(tagID, entries):
           json
         }}
     }}
-    '''
+    """
     return query_string
 
-def findTagID_Create (Tag_Identifier, tagName, dataType, endpoint_url, header, desc= "This was made using Python", create= True, verbose = False):
+
+def findTagID_Create(
+    Tag_Identifier: str,
+    tagName: str,
+    dataType: str,
+    endpoint_url: str,
+    header: dict,
+    desc: str = "This was made using Python",
+    create: bool = True,
+    verbose: bool = False,
+) -> list[str]:
+    """Find a tagID and create it if it does not already exist"""
     # Tag Identifier should proceed the tag
     # This is usually in reference to the connector being used
     # example: tagIdentifier.tagName, MQTT_Connector.ccam_sim_runID
 
-    if(verbose==True):print("find/create Tag ID start ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    if verbose:
+        print("find/create Tag ID start ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     my_query = build_TagList_Query(Tag_Identifier)
-    if(verbose==True):print(my_query)
+    if verbose:
+        print(my_query)
     result = request(my_query, endpoint_url, header)
-    tagList = result.get('data').get('tags')
+    tagList = result.get("data").get("tags")
 
     tagID = None
 
-    #search for tag in list
+    # search for tag in list
     for i in tagList:
-        if i['displayName'] == tagName:
-            tagID= i['id']
+        if i["displayName"] == tagName:
+            tagID = i["id"]
 
-    
-    #if tag not present create tag
-    if(create == True):
-        if (tagID == None):
-            if(verbose==True):print("\ntag not in list and will be added\n")
+    # if tag not present create tag
+    if tagID is None:
+        if create:
             my_Mutation = build_CreateTag_Mutation(tagName, dataType, desc)
-            if(verbose==True):print(my_Mutation)
             result = request(my_Mutation, endpoint_url, header)
-            if(verbose==True):print(result)
+
+            if verbose:
+                print("\ntag not in list and will be added\n")
+                print(my_Mutation)
+                print(result)
+
             result = request(my_query, endpoint_url, header)
-            tagList = result.get('data').get('tags')
+            tagList = result.get("data").get("tags")
 
             for i in tagList:
-                if i['displayName'] == tagName:
-                    tagID= i['id']
-    else:
-        if(tagID == None):
-            if(verbose==True):print("No Tag found and will not create")
-            return
+                if i["displayName"] == tagName:
+                    tagID = i["id"]
+        else:
+            if verbose:
+                print("No Tag found and will not create")
+            return [tagName, tagID]
 
+    if verbose:
+        print(f"\n{tagName} tagID: {tagID}\n")
 
-    if(verbose==True):print(f"\n{tagName} tagID: {tagID}\n")
-
-    if(verbose==True):print("find/create Tag ID end ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    if verbose:
+        print("find/create Tag ID end ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     return [tagName, tagID]
 
-def contains(small, big):
-    for i in range(len(big)-len(small)+1):
+
+def contains(small: str, big: str) -> bool:
+    """Return bool if small is in big"""
+    for i in range(len(big) - len(small) + 1):
         for j in range(len(small)):
-            if big[i+j] != small[j]:
+            if big[i + j] != small[j]:
                 break
         else:
             return True
     return False
 
-def remove_dup(a):
-   i = 0
-   while i < len(a):
-      j = i + 1
-      while j < len(a):
-         if a[i] == a[j]:
-            del a[j]
-         else:
-            j += 1
-      i += 1
 
-def build_entries(config, file, verbose=False):
+def remove_dup(a: list) -> None:
+    """Removes duplicates from list"""
+    i = 0
+    while i < len(a):
+        j = i + 1
+        while j < len(a):
+            if a[i] == a[j]:
+                del a[j]
+            else:
+                j += 1
+        i += 1
 
-    final_entries = [None] * len(config['Topic_toSMP'])
+
+def build_entries(config: list[str], file: str, verbose: bool = False) -> str:
+    """Build entries for batch upload to SMIP"""
+    final_entries = [None] * len(config["Topic_toSMP"])
     topic_count = 0
 
-
-    for t in config['Topic_toSMP']:
-        if(file != None):
+    for t in config["Topic_toSMP"]:
+        if file is not None:
             entries = []
 
-            if(verbose==True):
-                print('\n************************************************************************')
-                print(f'''Uploading topic: {t} \nFrom: {file}''') 
-                print('\n************************************************************************')
+            if verbose:
+                print(
+                    "\n************************************************************************"
+                )
+                print(f"""Uploading topic: {t} \nFrom: {file}""")
+                print(
+                    "\n************************************************************************"
+                )
 
-            with open(file, newline='') as f:
+            with open(file, newline="") as f:
                 reader = csv.reader(f)
                 data = list(reader)
-
-                #TODO confirm there isnt a bug in the MQTT Logger
-                #scan list for duplicates
                 remove_dup(data)
 
             for x in data:
-                if (x != []):
-                    y = x[0].split('_')
-                    if (contains(t,y)):
-                        dp = f'''{{value: "{x[1]}", timestamp: "{x[2]}", status: "0"}}'''
+                if x != []:
+                    y = x[0].split("_")
+                    if contains(t, y):
+                        dp = (
+                            f"""{{value: "{x[1]}", timestamp: "{x[2]}", status: "0"}}"""
+                        )
                         entries += [dp]
-                        if(verbose==True): print(dp)
-                        if(verbose==True): print(x)
+                        if verbose:
+                            print(dp)
+                        if verbose:
+                            print(x)
 
-
-        entries = ','.join(entries)
+        entries = ",".join(entries)
         final_entries[topic_count] = entries
         topic_count += 1
 
-
-    if(verbose==True):print(final_entries)
+    if verbose:
+        print(final_entries)
 
     return final_entries
 
-def SMP_auth():
+
+def SMP_auth() -> dict:
+    """SMIP Authentication"""
     dotenv.load_dotenv()
     endpoint_url = os.environ.get("endpoint_url")
     authenticator = os.environ.get("authenticator")
@@ -275,7 +351,9 @@ def SMP_auth():
 
     return header
 
-if __name__ == '__main__':
-    #SMP header setup
+
+if __name__ == "__main__":
+    """Request JWT"""
+    # SMP header setup
     header = SMP_auth()
     print(header)
